554f3ff1f120436dbbe18c770925b60f9ae356cb
gideonrynn <sarah.c.dillard@gmail.com>
2020-01-12 21:49:44 -0600

    updated index and initial routes

--- server.js
+++ server.js
@@ -1,13 +1,12 @@
-//this file runs the application
+//this server file runs the application
+
 // Dependencies
-// =============================================================
-var express = require("express");
+const express = require("express");
 var path = require("path");
 
 // Sets up the Express App
-// =============================================================
-var app = express();
-var PORT = 3000;
+const app = express();
+const PORT = 3000;
 
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
@@ -15,4 +14,23 @@ app.use(express.json());
 
 //Data is set up in the db/db.json file
 
+console.log(__dirname);
+
 //Set up our routes
+app.get("/", function(req, res) {
+    res.sendFile(path.join(__dirname, "public/index.html"));
+  });
+  
+app.get("/notes", function(req, res) {
+    res.sendFile(path.join(__dirname, "public/notes.html"));
+  });
+
+  // Will display all notes
+//     app.get("/db", function(req, res) {
+//     return res.json(db);
+//   });
+
+// Listener
+    app.listen(PORT, function() {
+    console.log("App listening on PORT " + PORT);
+  });
