eb0fd4a95d74d45c43d0f74552f6c76496d7a0be
gideonrynn <sarah.c.dillard@gmail.com>
2020-01-12 21:34:31 -0600

    updated index css, note navbar

--- server.js
+++ server.js
@@ -1,1 +1,18 @@
 //this file runs the application
+// Dependencies
+// =============================================================
+var express = require("express");
+var path = require("path");
+
+// Sets up the Express App
+// =============================================================
+var app = express();
+var PORT = 3000;
+
+// Sets up the Express app to handle data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+//Data is set up in the db/db.json file
+
+//Set up our routes
