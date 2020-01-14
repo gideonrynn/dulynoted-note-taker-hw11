5754718777c7cd1b2d894ef52aa085a974ffb8b9
gideonrynn <sarah.c.dillard@gmail.com>
2020-01-13 20:17:40 -0600

    updated db.json with test info, moved api and htmlRoutes to separate files

---
+++ routes/apiRoutes.js
@@ -0,0 +1,13 @@
+var path = require("path");
+
+module.exports = function(app) {
+
+    app.get("/api/notes", function(req, res) {
+        
+        // res.sendFile(path.normalize(__dirname, "../db", "/db.json"));
+    
+
+        
+    })
+
+}
--- server.js
+++ server.js
@@ -1,36 +1,27 @@
 //this server file runs the application
 
-// Dependencies
+// Sets up dependencies
 const express = require("express");
-var path = require("path");
+const path = require("path");
 
-// Sets up the Express App
+// Sets up express on port for localhost
 const app = express();
 const PORT = 3000;
 
-// Sets up the Express app to handle data parsing
+// Sets up  express to handle data parsing, including assets
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static(path.join(__dirname, "public")));
 
 //Data is set up in the db/db.json file
 
-//Set up our routes in the api and html routes files
-
-app.get("/", function(req, res) {
-    res.sendFile(path.join(__dirname, "public/index.html"));
-  });
-  
-app.get("/notes", function(req, res) {
-    res.sendFile(path.join(__dirname, "public/notes.html"));
-  });
-
-  // Will display all notes
-//     app.get("/db", function(req, res) {
-//     return res.json(db);
-//   });
+//Point to the route files, which both contain functions that take the app variable for express
+require("./routes/apiRoutes")(app);
+require("./routes/htmlRoutes")(app);
 
 // Listener
     app.listen(PORT, function() {
+
     console.log("App listening on PORT " + PORT);
+
   });
