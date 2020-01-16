c16cdc7eb6b20bfa0ffc89effe023d07cbeae199
gideonrynn <sarah.c.dillard@gmail.com>
2020-01-15 20:08:17 -0600

    created post that updates json object when title and notes are saved in browser

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,22 +1,39 @@
+// **Dependencies **//
 const path = require("path");
 const fs = require('fs');
+
+const dbFile = "db/db.json";
+const dbRaw = fs.readFileSync(path.resolve(dbFile));
+const db = JSON.parse(dbRaw);
 
 module.exports = function(app) {
 
     app.get("/api/notes", function(req, res) {
 
         //pull data from json file and send as response in json format
-        let notesDBraw = fs.readFileSync(path.resolve("db/db.json"));
-        let notesDB = JSON.parse(notesDBraw);
+        res.json(db);
 
-        res.json(notesDB);
+    });
 
-        //or
-        // fs.readFile("db/db.json", "utf8", function(err, data) 
-        // { if (err) { throw err;}
-        //   res.send(data) //or res.json(data);
-        // })
+    app.post("/api/notes", function(req, res) {
 
-  });
+        //add new note object to the existing db array
+        db.push(req.body);
+
+        //write new array to the db.json
+        fs.writeFile(dbFile, JSON.stringify(db, null, 2), function(err) {
+            if (err) {
+              return console.log(err);
+            }
+            console.log("Success!");
+          });
+
+    });
+
+    app.delete("/api/notes", function(req, res) {
+
+        console.log(req.body);
+    
+    })
  
 }
