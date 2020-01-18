2229ff32ec8a9bbe022f1a55b6fe3d993b4fc0b7
gideonrynn <sarah.c.dillard@gmail.com>
2020-01-17 20:15:35 -0600

    completed delete for when user deletes note in browser

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -6,6 +6,7 @@ const dbFile = "db/db.json";
 const dbRaw = fs.readFileSync(path.resolve(dbFile));
 let db = JSON.parse(dbRaw);
 
+
 module.exports = function(app) {
 
     app.get("/api/notes", function(req, res) {
@@ -18,13 +19,14 @@ module.exports = function(app) {
     app.post("/api/notes", function(req, res) {
 
         let newNote = req.body;
-        newNote['id'] = '';
-        console.log(newNote);
 
+        //add new key value pair to response: key = id, and value = '' (placeholder)
+        newNote['id'] = '';
+ 
         //add new note object to the existing db array
         db.push(newNote);
 
-        //run loop that will check the value 
+        //run loop that will check the id value and add number if blank
         for (let i = 0; i < db.length; i++) {
             if (db[i].id == "") {
                 db[i].id = i + 1;
@@ -38,10 +40,25 @@ module.exports = function(app) {
 
     });
 
-    app.delete("/api/notes", function(req, res) {
+    app.delete("/api/notes/:id", function(req, res) {
 
-        console.log(req.body);
-    
+        // console.log("We need to delete id:" + req.params.id);
+
+        let idDelete = req.params.id;
+
+        //create new array that does not contain the object with the id that should be deleted
+        const dbNew = db.filter(db => { return db.id != idDelete});
+
+        //run loop that will reorder ids of each object 
+        for (let i = 0; i < dbNew.length; i++) {
+            dbNew[i].id = i + 1;
+        };
+
+        //write updated db to db json file
+        fs.writeFile(dbFile, JSON.stringify(dbNew, null, 2), function(err) {
+            if (err) {return console.log(err); }
+            });
+
     })
  
 }
